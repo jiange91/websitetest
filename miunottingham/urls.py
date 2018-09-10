69aec0389549a1a5927fcf926d04fe09acee7390
zijian <eric@example.com>
2018-09-11 01:03:38 +0800

    改封面路由，改组织活动a-act跳转

--- miunottingham/urls.py
+++ miunottingham/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views
 
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('miunottingham/', include('activities.urls')),
